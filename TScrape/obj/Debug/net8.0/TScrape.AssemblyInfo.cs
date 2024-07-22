bc7f78d093b04051d09115a1a9376d5f1f95a475
Murat Eker <murateker9166@gmail.com>
2024-07-22 16:21:54 +0300

    16.21

--- TScrape/obj/Debug/net8.0/TScrape.AssemblyInfo.cs
+++ TScrape/obj/Debug/net8.0/TScrape.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TScrape")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ba3602c82cac638522a77d5b7dab295b376ff11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9df33e5b084acb56a9cc422f8a1085a297427527")]
 [assembly: System.Reflection.AssemblyProductAttribute("TScrape")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TScrape")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
