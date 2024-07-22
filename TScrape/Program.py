2c35fedd46fe4857a32750db9fb579d46a968a14
Murat Eker <murateker9166@gmail.com>
2024-07-22 11:50:55 +0300

    ╰（‵□′）╯

--- TScrape/Program.py
+++ TScrape/Program.py
@@ -18,7 +18,7 @@ def create_elastic_client():
 
 # Web'den veri çekme işlemi
 def scrape_web():
-    url = "https://www.trendyol.com/oyuncu-mouselari-x-c106088?sst=BEST_SELLER"  # Veri çekilecek web sitesi
+    url = "https://www.trendyol.com/oyuncu-mouselari-x-c106088?sst=SCORE"  # Veri çekilecek web sitesi
     response = requests.get(url)
     products = []
 
@@ -58,7 +58,7 @@ def scrape_web():
 def index_products(client, products, logger):
     actions = [
         {
-            "_index": "bseller",
+            "_index": "bests",
             "_source": {
                 "product_name": product.product_name,
                 "prices": product.prices,
@@ -72,8 +72,8 @@ def index_products(client, products, logger):
 
 # Elasticsearch'te index varsa kontrol eder, yoksa oluşturur
 def create_index_if_not_exists(client, logger):
-    if not client.indices.exists(index="bseller"):
-        client.indices.create(index="bseller", body={
+    if not client.indices.exists(index="bests"):
+        client.indices.create(index="bests", body={
             "mappings": {
                 "properties": {
                     "product_name": {"type": "text"},
@@ -87,7 +87,7 @@ def create_index_if_not_exists(client, logger):
 def search_products(client, search_text, logger):
     # Fuzzy search yerine daha geniş bir eşleme için multi_match query kullanıyoruz
     search_response = client.search(
-        index="bseller",
+        index="bests",
         body={
             "query": {
                 "multi_match": {
@@ -129,7 +129,7 @@ def main():
     # Web sitesinden ürünleri çeker
     products, soup = scrape_web()
 
-    flag_file_path = "flags/indexing_done_33.flag"  # Dosya oluşturmak için
+    flag_file_path = "flags/indexing_done_34.flag"  # Dosya oluşturmak için
 
     # Dosyanın oluşturulup oluşturulmadığını kontrol eder
     if not os.path.exists(flag_file_path):
