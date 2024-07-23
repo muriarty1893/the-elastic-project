d2f563089c55b704ff60be5f5b4976b01f85c95b
Murat Eker <murateker9166@gmail.com>
2024-07-23 15:26:50 +0300

    added gitignore

--- TScrape/obj/Debug/net8.0/TScrape.AssemblyInfo.cs
+++ TScrape/obj/Debug/net8.0/TScrape.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TScrape")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0420c167e3d7ba28a998cc54f57d3f47b675360e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edcb388862ddd1ba0857431c094e180cbb6c12a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TScrape")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TScrape")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
