0956ea35efad6af4f5009ff9dd0ceb06f2102173
Murat Eker <murateker9166@gmail.com>
2024-07-24 10:04:11 +0300

    trying_to_add_new_features

--- TScrape/Program.py
+++ TScrape/Program.py
@@ -6,10 +6,14 @@ import os
 import time
 
 class Product:
-    def __init__(self, product_name=None, prices=None, rating_count=None):
+    def __init__(self, product_name=None, prices=None, rating_count=None, dpi=None, rgb_lighting=None, mouse_type=None, button_count=None):
         self.product_name = product_name
         self.prices = prices or []
         self.rating_count = rating_count or []
+        self.dpi = dpi or []
+        self.rgb_lighting = rgb_lighting or []
+        self.mouse_type = mouse_type or []
+        self.button_count = button_count or []
 
 def create_elastic_client():
     return Elasticsearch([{'host': 'localhost', 'port': 9200, 'scheme': 'http'}])
@@ -28,6 +32,10 @@ def scrape_web():
             product_name_node = node.select_one("h3.prdct-desc-cntnr-ttl-w")
             price_node = node.select_one("div.prc-box-dscntd")
             rating_count_node = node.select_one("span.ratingCount")
+            dpi_node = node.select_one('span[title="Mouse Hassasiyeti (Dpi)"] + span.attribute-value > div.attr-name.attr-name-w')
+            rgb_lighting_node = node.select_one('span[title="RGB Aydınlatma"] + span.attribute-value > div.attr-name.attr-name-w')
+            mouse_type_node = node.select_one('span[title="Mouse Tipi"] + span.attribute-value > div.attr-name.attr-name-w')
+            button_count_node = node.select_one('span[title="Buton Sayısı"] + span.attribute-value > div.attr-name.attr-name-w')
 
             product_name = (
                 " ".join([
@@ -41,11 +49,19 @@ def scrape_web():
             if price:
                 price = float(price.replace("TL", "").replace(",", "").replace(".", ""))
             rating_count = rating_count_node.get_text().strip() if rating_count_node else None
+            dpi = dpi_node.get_text().strip() if dpi_node else None
+            rgb_lighting = rgb_lighting_node.get_text().strip() if rgb_lighting_node else None
+            mouse_type = mouse_type_node.get_text().strip() if mouse_type_node else None
+            button_count = button_count_node.get_text().strip() if button_count_node else None
 
             product = Product(
                 product_name=product_name,
                 prices=[price] if price else [],
-                rating_count=rating_count
+                rating_count=rating_count,
+                dpi=dpi,
+                rgb_lighting=rgb_lighting,
+                mouse_type=mouse_type,
+                button_count=button_count
             )
             products.append(product)
         
@@ -60,7 +76,11 @@ def index_products(client, products, logger):
             "_source": {
                 "product_name": product.product_name,
                 "prices": product.prices,
-                "rating_count": product.rating_count
+                "rating_count": product.rating_count,
+                "dpi": product.dpi,
+                "rgb_lighting": product.rgb_lighting,
+                "mouse_type": product.mouse_type,
+                "button_count": product.button_count
             }
         }
         for product in products
@@ -75,7 +95,11 @@ def create_index_if_not_exists(client, logger):
                 "properties": {
                     "product_name": {"type": "text"},
                     "prices": {"type": "float"},
-                    "rating_count": {"type": "keyword"}
+                    "rating_count": {"type": "keyword"},
+                    "dpi": {"type": "text"},
+                    "rgb_lighting": {"type": "text"},
+                    "mouse_type": {"type": "text"},
+                    "button_count": {"type": "text"}
                 }
             }
         })
@@ -91,7 +115,7 @@ def search_products(client, search_text, logger):
                         {
                             "multi_match": {
                                 "query": search_text,
-                                "fields": ["product_name^3", "rating_count"],
+                                "fields": ["product_name^3", "rating_count", "dpi", "rgb_lighting", "mouse_type", "button_count"],
                                 "fuzziness": "AUTO"
                             }
                         },
@@ -139,10 +163,18 @@ def search_products(client, search_text, logger):
         else:
             print(f"Rating Count: {product.get('rating_count', 'N/A')}")
             print("--------------------------------------------")
+        print(f"DPI: {product.get('dpi', 'N/A')}")
+        print(f"RGB Lighting: {product.get('rgb_lighting', 'N/A')}")
+        print(f"Mouse Type: {product.get('mouse_type', 'N/A')}")
+        print(f"Button Count: {product.get('button_count', 'N/A')}")
 
     print("Aggregation Results:\n--------------------------------------------")
     for bucket in search_response['aggregations']['price_ranges']['buckets']:
         print(f"Price range: {bucket['key']} - Doc count: {bucket['doc_count']}")
+
+def features():
+    print("praise")
+    
 
 def main():
     start_time1 = time.time()
