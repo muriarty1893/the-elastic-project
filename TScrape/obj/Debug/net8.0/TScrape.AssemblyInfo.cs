8a0ebef58f2b20eb06ba6e830504720af503fcf8
Murat Eker <murateker9166@gmail.com>
2024-08-11 17:56:39 +0300

    merge

--- TScrape/obj/Debug/net8.0/TScrape.AssemblyInfo.cs
+++ TScrape/obj/Debug/net8.0/TScrape.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TScrape")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55ffd745fb12dcd0bc2f35ba270ae17d857dabb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebf423cf202f780a6cec812f247b9d24631a93d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TScrape")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TScrape")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
